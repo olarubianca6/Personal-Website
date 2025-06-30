2b4d14f1db5288d9aa73d2e1c0b2509a17ec852f
Bianca <143421359+olarubianca6@users.noreply.github.com>
2025-06-30 17:32:36 +0300

    refactors

--- src/files/cv.js
+++ src/files/cv.js
@@ -1,10 +1,13 @@
 const cv = [
-  '2 years software development experience:',
+  '3 years software development and project management experience:',
+  '- Currently working in project management as a project support associate',
   '- QA Manual and Automation Testing Internship at Ness Digital Engineering (Java)',
-  '- Currently working as a freelance web developer',
-    "- Software Engineering master's student at Alexandru Ioan Cuza University (2024-2026),",
-    ' - 8 month course with trainer: ',
-      '  Python fundamentals and automated testing with Selenium',
+  '- Freelance web developer',
+    "- Software Engineering master's student at Alexandru Ioan Cuza University (2024-2026)",
+    '- BSc Project and Event Management at Canterbury Christ Church University',
+    'Other certifications: ',
+    '- 2 month Kotlin course taught by Google developers',
+    '- 8 month Python & automated testing course with trainers',
       ' - Udemy courses:',
     '    React, Next.js & Typescript',
     '    Machine Learning with Python using Jupyter'
